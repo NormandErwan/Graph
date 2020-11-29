17e5e64bd835d9344e54f4edb0e5e54e28473322
Erwan Normand <normand.erwan@protonmail.com>
2020-11-29 15:06:31 +0100

    :sparkles: Add Interval

---
+++ Assets/Scripts/Interval.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+namespace Graph
+{
+    [Serializable]
+    public sealed class Interval
+    {
+        public static readonly Interval Default = (DefaultMin, DefaultMax);
+
+        public static readonly float DefaultMax = 1;
+
+        public static readonly float DefaultMin = 0;
+
+        [SerializeField]
+        private float min = DefaultMin;
+
+        [SerializeField]
+        private float max = DefaultMax;
+
+        public Interval(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public float Distance => Max - Min;
+
+        public float Max { get => max; set => max = value; }
+
+        public float Min { get => min; set => min = value; }
+
+        public static implicit operator Interval((float min, float max) value)
+        {
+            return new Interval(value.min, value.max);
+        }
+
+        public static implicit operator (float min, float max)(Interval interval)
+        {
+            return (interval.Min, interval.Max);
+        }
+    }
+}
