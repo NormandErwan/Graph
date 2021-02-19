7f2cf01c38dde8accaa352c48fac8c3072051690
Erwan Normand <normand.erwan@protonmail.com>
2021-02-19 18:51:12 +0100

    :sparkles: Add LinearMapGpu.cs

---
+++ Assets/Scripts/LinearMapGpu.cs
@@ -0,0 +1,18 @@
+namespace Graph
+{
+    public struct LinearMapGpu
+    {
+        public LinearMapGpu(Map map)
+        {
+            Coefficient = map.Image.Distance / map.Domain.Distance;
+            DomainMin = map.Domain.Min;
+            ImageMin = map.Image.Min;
+        }
+
+        public float Coefficient { get; }
+
+        public float DomainMin { get; }
+
+        public float ImageMin { get; }
+    }
+}
